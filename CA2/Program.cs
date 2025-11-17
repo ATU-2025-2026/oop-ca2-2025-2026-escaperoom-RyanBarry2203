a3bd31613afbdd5398f3468f759f23332c568b6b
Ryan Barry <S00250496@atu.ie>
2025-11-17 11:27:30 +0000

    First Push

---
+++ CA2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace CA2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //vashvcjkb
+            //vcjhyewvvejh
+        }
+    }
+}
